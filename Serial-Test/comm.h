5a3ee25e9fc4db5fc7af9f957b0b504dfd7adad3
Josh Osborne <Josborne-cc@conestogac.on.ca>
2015-11-04 19:51:16 -0500

    upload of comm class

---
+++ Serial-Test/comm.h
@@ -0,0 +1,29 @@
+/*
+Purpose: Weather Station Module
+	 Serial Communication for Beaglebone
+	 Class Definition
+ Author: Josh Osborne
+Version: 00
+*/
+
+#ifndef _COMM_H
+#define _COMM_H
+
+#define BAUD_RATE B9600
+#define SERIAL_PORT "/dev/ttyO4"
+
+class Serial_Comm
+{
+	public:
+		Serial_Comm();
+		//~Serial_Comm();
+		void Open_Port();
+		void Initialize_Port();
+		void Close_Port();
+		void Write_Port(string);
+	private:
+		bool port_status;
+		int fd
+};
+
+#endif
